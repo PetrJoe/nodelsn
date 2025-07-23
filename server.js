fe53fe5b1625eab1c0ec334d4ed5613f2ea53cc0
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2025-07-23 10:56:18 +0000

    space removed

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello, World!\n');
+// });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
+// server.listen(3000, () => {
+//   console.log('Server running at http://localhost:3000/');
+// });
+
+
+
+
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
