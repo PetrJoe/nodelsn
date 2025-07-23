55b81a1679aeb7a694c169a4fb4915c06d238ee3
PetrJoe <akinjossy1298@gmail.com>
2025-07-23 11:30:20 +0100

    space removed

--- index.js
+++ index.js
@@ -2,50 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.use(express.json());
 
 // In-memory dummy database
